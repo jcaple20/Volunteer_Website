089b4b132c3148d363249907b8533f6306504e84
jcaple20 <47284720+jcaple20@users.noreply.github.com>
2020-02-16 22:57:26 -0600

    Added overlay forms and changed page colors
    Currently, the overlays' close outs don't function.

--- account_page.js
+++ account_page.js
@@ -15,8 +15,56 @@ function openSection(evt, sectionNum) {
     evt.currentTarget.className += " active";
     }
 
-// funtions for section 1 from
-function showInput() {
-    document.getElementById('display').innerHTML = 
-                document.getElementById("fname").value;
+// Get the first modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+// Get the second modal
+var modal2 = document.getElementById("myModal2");
+
+// Get the button that opens the second modal
+var btn2 = document.getElementById("myBtn2");
+
+// Get the <span> element that closes the second modal
+var span2 = document.getElementsByClassName("close")[0];
+
+// When the user clicks the second button, open the second modal 
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+// When the user clicks on second <span> (x), close the second modal
+span.onclick = function() {
+  modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the second modal, close it
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
 }
